2873896c1b3150870fca6f2ffff98bb7ecceb1a0
kimsoyol <naywinlhaing@gmail.com>
2023-11-22 01:35:17 +0630

    add blog details page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -61,19 +61,20 @@ const page = () => {
 
   return (
     <section className="text-primary px-20 max-w-[1600px] m-auto py-20">
-      <div className="flex justify-between items-center">
-        <p className="font-serif font-light text-xl sm:text-4xl md:text-5xl lg:text-6xl">Our Blogs</p>
-        <div className="relative hidden md:block">
+      <div className="flex justify-between items-center gap-40">
+        <p className="font-serif font-light text-xl sm:text-4xl md:text-5xl lg:text-6xl flex-none">Our Blogs</p>
+        <div className="relative hidden md:block max-w-[600px] w-full flex-auto">
           <input
             type="search"
-            className="block sm:w-64 md:w-96 lg:w-[666px] p-4 ps-10 text-sm border-primary border placeholder:text-primary outline-none"
+            className="block w-full p-4 ps-10 text-sm border-primary border placeholder:text-primary outline-none"
             placeholder="Search Articles..."
             required
+            autoComplete="off"
           />
 
           <div className="absolute inset-y-0 end-0 flex items-center ps-3 pointer-events-none pr-6">
             <svg
-              className="w-4 h-4 text-primary"
+              className="w-6 h-6 text-primary"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -90,7 +91,7 @@ const page = () => {
           </div>
         </div>
       </div>
-
+    
       <div className="flex gap-5 pt-20 flex-wrap justify-center">
         {blogs.map((blog) => (
           <div
