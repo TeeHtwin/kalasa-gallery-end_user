2f87e3d3490bdf46d0320fafbd73d6b908e3b875
kimsoyol <naywinlhaing@gmail.com>
2023-11-22 22:35:38 +0630

    fix pagination

--- src/app/components/pagination/Pagination.tsx
+++ src/app/components/pagination/Pagination.tsx
@@ -118,6 +118,7 @@ function PaginationArrow({
       </div>
     ) : (
       <div className="flex">
+        <p>Next</p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -126,7 +127,6 @@ function PaginationArrow({
         >
           <path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" />
         </svg>
-        <p>Next</p>
       </div>
     );
 
