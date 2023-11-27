09f5b94264891a4dc5093572ad5bbf03f0f8680d
kimsoyol <naywinlhaing@gmail.com>
2023-11-27 15:00:11 +0630

    add not-found page

--- src/app/components/fullscreenImage/fullscreenImage.jsx
+++ src/app/components/fullscreenImage/fullscreenImage.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import useKeypress from 'react-use-keypress'
+
 
 function FullscreenImage({ src }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,16 +15,29 @@ function FullscreenImage({ src }) {
     document.body.style.overflow = isOpen ? "hidden" : "unset";
   }, [isOpen]);
 
+  useKeypress('Escape', () => {
+    openPhoto()
+  })
 
   return isOpen ? (
-    <Image
-      src={src}
-      width={728}
-      height={480}
-      alt="collection poster"
-      className="block absolute z-50 top-0 left-0 w-screen h-screen bg-contain bg-no-repeat bg-center bg-black object-contain"
-      onClick={openPhoto}
-    />
+    <>
+      <Image
+        src={src}
+        width={728}
+        height={480}
+        alt="collection poster"
+        className="block fixed z-50 inset-0 w-screen h-screen bg-black object-contain"
+
+      />
+      <Image
+        src="/icons/close_arrow.svg"
+        width={40}
+        height={40}
+        alt="zoom arrow"
+        className="block fixed top-3 right-3 z-50"
+        onClick={openPhoto}
+      />
+    </>
   ) : (
     <div className="object-cover w-full relative">
       <Image
@@ -33,20 +48,14 @@ function FullscreenImage({ src }) {
         className="object-cover w-full"
         onClick={openPhoto}
       />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="w-6 h-6 absolute top-0 right-0"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-        />
-      </svg>
+      <Image
+        src="/icons/zoom_arrow.svg"
+        width={40}
+        height={40}
+        alt="zoom arrow"
+        className="hidden sm:block absolute top-3 right-3"
+        onClick={openPhoto}
+      />
     </div>
   );
 }
