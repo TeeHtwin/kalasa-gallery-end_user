006fab841b0416833df83683bdd571bb3e4a2f62
Yin Yin Kyaw <yinyinkyaw.zotefamily@gmail.com>
2024-01-05 23:33:44 +0630

    [implement] blog detail

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import Layout from "../components/common/Layout";
-import SectionHeader from "../components/common/SectionHeader";
 
-import ExhibitionCard from "../components/cards/ExhibitionCard";
-import Pagination from "../components/pagination/Pagination";
 import HeroSearch from "../components/HeroSearch/HeroSearch";
-import { Event } from "@/types";
 import EventList from "../components/event/EventList";
-// import Pagination from "../components/common/pagination";
 
 export default function page() {
   return (
