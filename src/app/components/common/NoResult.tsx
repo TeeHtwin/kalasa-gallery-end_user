6c7b37273a8e745d463362126f47979d2b177166
khantlinnyan <khantlinnyan011027@gmail.com>
2024-01-16 22:44:09 +0630

    style: noResultsFound

--- src/app/components/common/NoResult.tsx
+++ src/app/components/common/NoResult.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import noResultFound from "@/../public/icons/noResultFound.png";
+import Image from "next/image";
 
 const NoResult = () => {
   return (
-    <div className=" flex justify-center items-center h-full w-full text-primary">
-      No Results Found.
+    <div className="flex flex-col gap-4 my-10 justify-center items-center h-full w-full text-primary">
+      <h2>No Results Found.</h2>
+      <Image
+        src={noResultFound}
+        alt="no result found"
+        width={150}
+        height={150}
+      />
     </div>
   );
 };
