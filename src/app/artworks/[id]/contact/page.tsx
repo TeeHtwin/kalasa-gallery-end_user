6e609f8a54f13d4829f83cb967196bda54f7fe29
kimsoyol <naywinlhaing@gmail.com>
2024-03-15 22:13:02 +0700

    fix contact inquiry

--- src/app/artworks/[id]/contact/page.tsx
+++ src/app/artworks/[id]/contact/page.tsx
@@ -1,10 +1,20 @@
-"use client";
-
 import Breadcrumb from "@/app/components/breadcrumb/Breadcrumb";
 import ContactUs from "@/app/components/contactUs/ContactUs";
 import { useSearchParams } from "next/navigation";
+import { API } from "@/utils/domain";
+import { Artwork } from "@/types";
+import data from "@/data";
 
-const Page = ({ params }: { params: { id: string } }) => {
+
+const Page = async({ params }: { params: { id: string } }) => {
+  const { data: artwork }: { data: Artwork } = await fetch(
+    `${API}/api/enduser/artwork/${params?.id}`
+  )
+    .then((res) => res.json())
+    .catch((error) => console.log("artwork detail error", error));
+
+  
+  
   return (
     <>
       <div className="wrapper px-4 sm:px-20">
@@ -21,7 +31,7 @@ const Page = ({ params }: { params: { id: string } }) => {
           ]}
         />
       </div>
-      <ContactUs />
+      <ContactUs name={artwork.name} />
     </>
   );
 };
