fedbbe5ecfaef8c4f7c99a41d2925854d4681368
kimsoyol <naywinlhaing@gmail.com>
2024-03-19 14:31:38 +0700

    fix home api

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,13 +5,13 @@ import { base_url } from "@/fetchers/api";
 export async function getHomeData() {
   try {
     const response = await fetch(
-      `${base_url}/api/enduser/home`
+      `https://api.kalasa.gallery/api/enduser/home`
     )
     const data = await response.json()
 
     return data.data
   } catch (error) {
     console.error("API Error:", error);
-    throw new Error("Failed to fetch User");
+    throw new Error("Failed to fetch Home Page");
   }
 }
