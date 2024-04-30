da251bcf1cb937dac7a38a0eb40cdd9f819d9d3a
khantlinnyan <khantlinnyan011027@gmail.com>
2024-04-30 23:52:17 +0630

    Production

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,7 +4,7 @@ import { base_url } from "@/fetchers/api";
 
 export async function getHomeData() {
   try {
-    const response = await fetch(`${base_url}/api/enduser/home`);
+    const response = await fetch(`https://api.kalasa.gallery/api/enduser/home`);
     const data = await response.json();
 
     return data.data;
